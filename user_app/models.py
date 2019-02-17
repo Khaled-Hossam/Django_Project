424bef18f46ad87c3b26375a74f3107fcc1aecd6
mahydit <mahydti_anber@outlook.com>
2019-02-17 03:59:55 +0200

    fixing case senstivity bugs

--- user_app/models.py
+++ user_app/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
 def pics_directory_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    # file will be uploaded to MEDIA_ROOT/profile_pics/<user.id>
     return 'profile_pics/{}'.format(instance.user.id)
 
 # in User model there is an attribute called is_superuser and is_staff
@@ -18,3 +17,9 @@ class UserProfileInfo(models.Model):
 
     def __str__(self):
         return self.user.get_full_name()
+
+    def clean_username(self):
+        return self.username.lower()
+
+    def clean_email(self):
+        return self.email.lower()
