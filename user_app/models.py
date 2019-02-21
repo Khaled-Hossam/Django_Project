e895ac02ac688e3774cead447bdefab346529414
mahydit <mahydti_anber@outlook.com>
2019-02-21 05:30:04 +0200

    adding update file part

--- user_app/models.py
+++ user_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from user_app.storage import OverwriteStorage
 
 def pics_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/profile_pics/<user.id>
@@ -12,9 +13,8 @@ def pics_directory_path(instance, filename):
 
 class UserProfileInfo(models.Model):
     user = models.OneToOneField(User)
-    profile_picture = models.ImageField(upload_to=pics_directory_path,blank=True)
+    profile_picture = models.ImageField(upload_to=pics_directory_path,blank=True, storage=OverwriteStorage())
     user_username = models.CharField(max_length=40,default='null')
-    # TODO: add gender
 
     def __str__(self):
         return self.user.get_full_name()
