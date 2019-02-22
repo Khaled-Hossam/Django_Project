b3642c6c447e0241c19efc0d78946e853bbcaa22
mahydit <mahydti_anber@outlook.com>
2019-02-22 11:40:47 +0200

    Adding user authentication

--- car/views.py
+++ car/views.py
@@ -3,19 +3,26 @@ from .forms import reservationForm
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from user_app.models import UserProfileInfo
+from django.contrib.auth import authenticate # added by Mahydit to authenticate
+from django.core.urlresolvers import reverse # added by Mahydit to authenticate
+
 # Create your views here.
 
 
 def reservation(request):
-    form = reservationForm()
-    if request.method == "POST":
-        form = reservationForm(request.POST)
-        if form.is_valid():
-                obj=form.save(commit=False)
-                obj.user=UserProfileInfo.objects.get(id=1)
-                obj.save()
-                return HttpResponse("<div class='alert alert-success '>Booking Done </div>")
-    return render(request, 'car.html', {'form':form})
+        if request.user.is_authenticated: # added by Mahydit to authenticate
+                form = reservationForm()
+                if request.method == "POST":
+                        form = reservationForm(request.POST)
+                        if form.is_valid():
+                                obj=form.save(commit=False)
+                                obj.user=UserProfileInfo.objects.get(id=1)
+                                obj.save()
+                                return HttpResponse("<div class='alert alert-success '>Booking Done </div>")
+                return render(request, 'car.html', {'form':form})
 
-    #form = form.save(commit=False)
-    #form.save()
+                #form = form.save(commit=False)
+                #form.save()
+
+        else: # added by Mahydit to authenticate
+                return HttpResponseRedirect(reverse("user_app:user_login"))    # added by Mahydit to authenticate 
